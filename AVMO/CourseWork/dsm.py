c2cf60b045dfdab62bf8c16cc803c639f056ddc5
Menchshikov Danil <freeestyler231@gmail.com>
2022-05-18 17:13:44 +0700

    Added full avmo course work

--- AVMO/CourseWork/dsm.py
+++ AVMO/CourseWork/dsm.py
@@ -1,23 +1,20 @@
 from my_fraction import Fraction
 
 basis = []
-const = 999999
+checkfunction = []
 
-
-# функция создает решение для определенного базиса
-def make_solution_by_x(bs, coeff, solutions, matrix):
+def makeSolutionX(bs, coeff, solutions, matrix):
     solution = []
-    for _ in range(0, len(matrix[0])):
-        solution.append(Fraction(0, 1))
+    for i in range(0, len(matrix[0])):
+        solution.append(Fraction(0,1))
     for item in bs:
         solution[item[1]] = coeff[0][item[0]]
     solutions.append(solution)
 
 
-# находит наименьший в строке свободных членов(столбец "1")
-def find_greatest_in_solution(solution):
-    min = Fraction(const)
-    index = const
+def findGreatestSolution(solution):
+    min = Fraction(999)
+    index = -1
     for i in range(0, len(solution[0])):
         if solution[0][i] < Fraction(0, 1):
             if solution[0][i] < min:
@@ -26,12 +23,11 @@ def find_greatest_in_solution(solution):
     return index
 
 
-# Находит элемент взяв минимальный из строки свободных членов и из столбца SO(тут же его и считаем)
-def find_solve(solution, matrix, function):
-    index = find_greatest_in_solution(solution)
-    so = Fraction(-const)
-    i = const
-    if(index != const):
+def findSolve(solution, matrix, function):
+    index = findGreatestSolution(solution)
+    so = Fraction(-999)
+    i = -1
+    if(index != -1):
         for k in range(0, len(matrix[index])):
             if(matrix[index][k] < Fraction(0, 1)):
                 if function[0][k] / matrix[index][k] > so:
@@ -40,9 +36,9 @@ def find_solve(solution, matrix, function):
     return index, i
 
 
-def find_negative_in_z(function):  # нахождение отрицательных чисел в строке Z
+def findNegative(function):
     max = Fraction(0)
-    j = const
+    j = -1
     for i in range(0, len(function[0]) - 1):
         if Fraction(0, 1) > function[0][i]:
             if function[0][i] < max:
@@ -51,56 +47,64 @@ def find_negative_in_z(function):  # нахождение отрицательн
     return j
 
 
-def find_removable_basis_var(i):  # Находим базис, который нужно будет удалить
+def findPrevious(i):
     for item in basis:
         if item[0] == i:
             return item
 
-
-# проверка на то, является ли элемент по строке или столбцу базисным
-def column_or_row_in_basis(column, row):
+def findPreviousByJ(j):
     for item in basis:
-        if item[1] == column or item[0] == row:
+        if item[1] == j:
+            return item
+
+def rowInBasis(row):
+    for item in basis:
+        if item[0] == row:
             return True
     return False
 
 
-# добавляем найденный базис в массив базисов
-def create_basis(matrix, solution, function):
+def columnInBasis(column):
+    for item in basis:
+        if item[1] == column:
+            return True
+    return False
+
+
+def createBasis(matrix, solution, function):
     for i in range(0, len(matrix)):
-        if column_or_row_in_basis(i, 0):
+        if rowInBasis(i):
             continue
         for j in range(0, len(matrix[i])):
-            if column_or_row_in_basis(0, j):
+            if columnInBasis(j):
                 continue
             basis.append((i, j))
-            jordan(
+            transformationJordan(
                 basis[len(basis)-1], matrix, solution, function)
             break
 
 
-def jordan(element, matrix, solution, function):  # Метод Джордана
+def transformationJordan(element, matrix, solution, function):
     i = element[0]
     j = element[1]
-    element_of_matrix = matrix[i][j]
+    elementMatrix = matrix[i][j]
     for k in range(0, len(matrix[0])):
-        matrix[i][k] /= element_of_matrix
-    solution[0][i] /= element_of_matrix
+        matrix[i][k] /= elementMatrix
+    solution[0][i] /= elementMatrix
 
     for c in range(0, len(matrix)):
         if c != i:
-            new_element_of_matrix = matrix[c][j]
+            newElementMatrix = matrix[c][j]
             for k in range(0, len(matrix[0])):
-                matrix[c][k] -= matrix[i][k]*new_element_of_matrix
-            solution[0][c] -= solution[0][i]*new_element_of_matrix
-    new_element_of_matrix = function[0][j]
+                matrix[c][k] -= matrix[i][k]*newElementMatrix
+            solution[0][c] -= solution[0][i]*newElementMatrix
+    newElementMatrix = function[0][j]
     for c in range(0, len(function[0]) - 1):
-        function[0][c] -= matrix[i][c] * new_element_of_matrix
-    function[0][len(function[0]) - 1] -= solution[0][i]*new_element_of_matrix
+        function[0][c] -= matrix[i][c] * newElementMatrix
+    function[0][len(function[0]) - 1] -= solution[0][i]*newElementMatrix
 
 
-# проверка на то, является ли элемент базисным, тоесть под ним или над ним нули
-def is_basis(matrix, column, row):
+def isBasis(matrix, column, row):
     for i in range(0, len(matrix)):
         if i == row:
             continue
@@ -110,146 +114,172 @@ def is_basis(matrix, column, row):
     return True
 
 
-def print_matrix(matrix, solution, function):  # вывод матрицы
-    print("\n\n")
-    print("Simplex table: ")
-    for i in range(0, len(matrix[0])):
-        print("%-15s" % ('x' + str(i + 1)), end="")
-    print("")
-    print("______________________________________________________________")
+def findAndCreateBasisVar(matrix, function, solution):
     for i in range(0, len(matrix)):
-        print("\n", end="")
-        for j in range(0, len(matrix[i])):
-            print("%-15s" % (str(matrix[i][j])), end="")
-        print(solution[0][i], end="        ")
-    print("\n")
-    print("______________________________________________________________")
-    for i in range(0, len(function[0])):
-        print("%-15s" % (str(function[0][i])),  end="")
-    print("")
-
-
-# нахождение симплекс отношения(похожа на функцию выше, но нужна для множества решений)
-def find_simplex_ratio(matrix, function, j):
-    so = Fraction(999)
-    i = const
-    for k in range(0, len(matrix)):
-        if matrix[k][j] > Fraction(0, 1):
-            print(function[0][k]/matrix[k][j])
-            if function[0][k]/matrix[k][j] < so:
-                so = function[0][k]/matrix[k][j]
-                i = k
-    return i
-
-
-def in_basis(j):  # проверка на то, находится ли столбец в базисе
-    for item in basis:
-        if item[1] == j:
-            return True
-    return False
-
-
-# проверка на то, есть ли множество решений в симплекс таблице
-def check_multiply_solutions(function):
-    j = const
-    for i in range(0, len(function[0])-1):
-        if function[0][i] == Fraction(0, 1) and not(column_or_row_in_basis(i, -const)):
-            j = i
-    return j
-
-
-# основная функция, здесь происходит к поиск разрешающего элемента, и его преобразование в базис
-def matrix_decision(solution, matrix, functionZ, solutions):
-    while(True):
-        if find_solve(solution, matrix, functionZ) != (const, const):
-            i, j = find_solve(solution, matrix, functionZ)
-            if j == const:
-                print("Система не ограничена")
-                break
-            removable_basis_var = find_removable_basis_var(i)
-            basis.remove(removable_basis_var)
-            basis.append((i, j))
-            basis.sort()
-            jordan((i, j), matrix, solution, functionZ)
-            make_solution_by_x(basis, solution, solutions, matrix)
-            print_matrix(matrix, solution, functionZ)
-            print("\nZ = ", (functionZ[0][len(functionZ[0]) - 1]))
-        else:
-            if check_multiply_solutions(functionZ) != const:
-                j = check_multiply_solutions(functionZ)
-                i = find_simplex_ratio(matrix, functionZ, j)
-                removable_basis_var = find_removable_basis_var(i)
-                basis.remove(removable_basis_var)
-                basis.append((i, j))
-                basis.sort()
-                jordan((i, j), matrix, solution, functionZ)
-                make_solution_by_x(basis, solution, solutions, matrix)
-                print_matrix(matrix, solution, functionZ)
-                multiply_sol = []
-                for i in range(0, len(solutions[0])):
-                    multiply_sol.append(
-                        Fraction(-1, 1)*solutions[len(solutions)-2][i] + solutions[len(solutions)-1][i])
-                print("Solution = [", end="")
-                for i in range(0, len(multiply_sol)):
-                    if multiply_sol[i] > Fraction(0, 1):
-                        print(
-                            f"{solutions[len(solutions)-2][i]} + {multiply_sol[i]}k", end="")
-                    else:
-                        print(
-                            f"{solutions[len(solutions)-2][i]} - {Fraction(-1,1)*multiply_sol[i]}k", end="")
-                    if i < len(multiply_sol) - 1:
-                        print("; ", end="")
-                print("]")
-            break
-
-
-def createMatrix():  # считывание матрицы
-    with open("test.txt") as f:
-        function_z_in_file = f.readline()
-        solution_in_file = f.readline()
-        matrix_in_file = f.readlines()
-
-    function_z = []
-    function_z.append([Fraction(x) for x in map(lambda item: int(item),
-                                                function_z_in_file.split(" "))])
-    for i in range(0, len(function_z[0]) - 1):
-        function_z[0][i] *= Fraction(-1)
-    print(function_z)
-    solution = []
-    solution.append([Fraction(x) for x in map(
-        lambda item: int(item), solution_in_file.split(" "))])
-    matrix = []
-    for line in matrix_in_file:
-        matrix.append([Fraction(x) for x in map(
-            lambda item: int(item), line.split(" "))])
-
-    print_matrix(matrix, solution, function_z)
-    for i in range(0, len(matrix)):  # findAndCreateBasisVar
         for j in range(0, len(matrix[i])):
             if(matrix[i][j] == Fraction(1, 1) or matrix[i][j] == Fraction(-1, 1)):
-                if is_basis(matrix, j, i) and function_z[0][j] == Fraction(0, 1):
+                if isBasis(matrix, j, i) and function[0][j] == Fraction(0, 1):
                     basis.append((i, j))
                     if(matrix[i][j] == -1):
                         for k in range(0, len(matrix[i])):
                             matrix[i][k] *= Fraction(-1)
                         solution[0][i] *= Fraction(-1)
-    print_matrix(matrix, solution, function_z)
+
+
+
+def printFunction(function):
+    print("Z", end="     ")
+    print(function[0][len(function[0]) - 1], end="")
+    for i in range(0, len(function[0])-1):
+        print("     ", function[0][i], end="")
+
+
+def printMatrix(matrix, solution, function):
+    print("")
+    print("б.п", end="    ")
+    print("1", end="    ")
+    for i in range(0, len(matrix[0])):
+        print("x" + str(i + 1), end="      ")
+    print("")
+    for i in range(0, len(matrix)):
+        print("\n", end="      ")
+        print(solution[0][i], end="      ")
+        for j in range(0, len(matrix[i])):
+            print(matrix[i][j], end="      ")
+    print("")
+    printFunction(function)
+    print("")
+
+def findSimplexRatio(matrix, function, j):
+    so = Fraction(999)
+    i = -1
+    for k in range(0, len(matrix)):
+        if matrix[k][j] > Fraction(0,1):
+            if function[0][k]/matrix[k][j] < so:
+                so = function[0][k]/matrix[k][j]
+                i = k
+
+    return i
+
+def printSol(solutions, checkfunction):
+    for i in range(0,len(solutions[0])):
+        if checkfunction[i]:
+            print(f"x{i+1}={solutions[len(solutions)-1][i]}", end=" ")
+
+def inBasis(j, bs):
+    for item in bs:
+        if item[1] == j:
+            return True
+    return False
+
+def checkMultipleSolution(function, bs):
+    j = -1
+    for i in range(0,len(function[0])-1):
+        if function[0][i] == Fraction(0,1) and not(inBasis(i, bs)):
+            j = i
+    return j
+
+def printMulSolution(solutions, chFunction):
+    fsl = []
+    for i in range(0, len(solutions[0])):
+        #(1-λ)X1 + VX2 =X1 - VX1 + VX2 = X1 + λ(-X1+X2)
+        fsl.append(solutions[len(solutions)-2][i]*Fraction(-1) + solutions[len(solutions) -1][i])
+    for i in range(0, len(fsl)):
+        if solutions[len(solutions)-2][i] == Fraction(0):
+            if fsl[i]==Fraction(0):
+                print(f"x{i+1}=0")
+            else:
+                print(f"x{i+1}={fsl[i]}λ")
+        elif fsl[i] == Fraction(0):
+            print(f"x{i+1}={fsl[i]}λ")
+        elif fsl[i] == Fraction(0) and solutions[len(solutions)-2][i] == Fraction(0):
+            print(f"x{i+1}=0")
+        else:
+            print(f"x{i+1}={solutions[len(solutions)-2][i]}", end="")
+            if fsl[i] < Fraction(0):
+                print(f" - {fsl[i]*Fraction(-1)}λ")
+            elif fsl[i] > Fraction(0):
+                print(f" + {fsl[i]}λ")
+            else:
+                print("0")
+
+
+def createMatrix():
+    with open("2.txt") as f:
+        functionZ_in_file = f.readline()
+        solution_in_file = f.readline()
+        matrix_in_file = f.readlines()
+
+    functionZ = []
+    functionZ.append([Fraction(x) for x in map(lambda item: int(item),
+                                               functionZ_in_file.split(" "))])
+    for i in range(0, len(functionZ[0]) - 1):
+        functionZ[0][i] *= Fraction(-1)
+        if functionZ[0][i] != Fraction(0):
+            checkfunction.append(True)
+        else:
+            checkfunction.append(False)
+    print(functionZ)
+    matrix = []
+    for line in matrix_in_file:
+        matrix.append([Fraction(x) for x in map(
+            lambda item: int(item), line.split(" "))])
+    solution = []
+    solution.append([Fraction(x) for x in map(
+        lambda item: int(item), solution_in_file.split(" "))])
+    
+
+    printMatrix(matrix, solution, functionZ)
+    findAndCreateBasisVar(matrix, functionZ, solution)
+    printMatrix(matrix, solution, functionZ)
     if len(basis) != len(matrix):
-        create_basis(matrix, solution, function_z)
-        print_matrix(matrix, solution, function_z)
-    if find_negative_in_z(function_z) != const:
+        createBasis(matrix, solution, functionZ)
+        printMatrix(matrix, solution, functionZ)
+    if findNegative(functionZ) != -1:
         print("Двойственно недопустимая")
         return
-    print_matrix(matrix, solution, function_z)
-
+    printMatrix(matrix, solution, functionZ)
+    print(f"\nБазис: {basis}")
+    
     solutions = []
-    make_solution_by_x(basis, solution, solutions, matrix)
-    matrix_decision(solution, matrix, function_z, solutions)
+    makeSolutionX(basis, solution, solutions, matrix)
+    while(True):
+        if findSolve(solution, matrix, functionZ) != (-1, -1):
+            i, j = findSolve(solution, matrix, functionZ)
+            if j == -1:
+                print("Система не ограничена")
+                break
+            oldBasisVar = findPrevious(i)
+            basis.remove(oldBasisVar)
+            basis.append((i, j))
+            basis.sort()
+            transformationJordan((i, j), matrix, solution, functionZ)
+            makeSolutionX(basis, solution, solutions, matrix)
+            printMatrix(matrix, solution, functionZ)
+            print(f"\nБазис: {basis}")
+            printSol(solutions, checkfunction)
+        else:
+            if checkMultipleSolution(functionZ, basis) != -1:
+                j = checkMultipleSolution(functionZ, basis)
+                i = findSimplexRatio(matrix, functionZ, j)
+                oldBasisVar = findPrevious(i)
+                basis.remove(oldBasisVar)
+                basis.append((i, j))
+                basis.sort()
+                transformationJordan((i, j), matrix, solution, functionZ)
+                makeSolutionX(basis, solution, solutions, matrix)
+                printMatrix(matrix, solution, functionZ)
+                print(f"\nbasis: {basis}")
+                printMulSolution(solutions, checkfunction)
+                print("\n Z = ", (functionZ[0][len(functionZ[0]) - 1]))
+            else:
+                printSol(solutions, checkfunction)
+                print("\n Z = ", (functionZ[0][len(functionZ[0]) - 1]))
+            break
 
 
 def main():
     createMatrix()
-
 
 if __name__ == "__main__":
     main()
